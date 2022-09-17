e0b3bb3a4e0fd5678567321610e58e6d87ba7e9e
EmekaG <nellychukwemeka@gmail.com>
2022-09-16 20:43:13 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - to determine if an integer is positive or negative
+ */
+int main void
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
