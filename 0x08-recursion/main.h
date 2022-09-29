4f0838bef30c06adf6769909b5ce94668a95edb2
EmekaG <nellychukwemeka@gmail.com>
2022-09-28 20:05:17 -0700

    main.h

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+/**
+ * file - main.h files
+ * description: prototypes as header files for my project
+ */
+
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strien_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+#endif
