94987e90da2c59ac163fd716fb9bab134f53e987
EmekaG <nellychukwemeka@gmail.com>
2022-10-10 14:15:54 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * file - 0-object_like_macro.h
+ * description: header file that defines a macro named
+ *              SIZE as an abbv. for the token 1024
+ */
+
+#define SIZE 1024
+
+#endif
