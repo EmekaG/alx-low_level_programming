45a7ff5124699fc23a716cbe8cea5eaaefd1eb0e
EmekaG <nellychukwemeka@gmail.com>
2022-10-12 15:14:00 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,10 +5,10 @@
  * @name: name to be printed
  * @f: a pointer to a function that prints a name
  */
-void print_name(char *name, void(*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
-	return;
+		return;
 
 	f(name);
 }
